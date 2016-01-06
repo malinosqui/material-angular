683ddfd8e3f9fe6b3fd6cd84b4b6edee6b9a5f1e
Gabriel Malinosqui <gabrielmalinosqui@gmail.com>
2016-01-06 20:40:43 -0200

    add tranning creation

--- app/app.js
+++ app/app.js
@@ -1,32 +1,70 @@
 (function (angular) {
-	'use strict';
+    'use strict';
 
-	angular.module('appMain', ['ngRoute', 'ngMaterial', 'ngAnimate'])
-		.config(function ($mdThemingProvider) {
-			$mdThemingProvider.theme('default')
-				.primaryPalette('blue-grey')
-				.accentPalette('red')
-				.warnPalette('red')
-				.backgroundPalette('grey');
-			$mdThemingProvider.definePalette('whitePalette', {
-				'50': 'fff',
-				'100': 'fff',
-				'200': 'fff',
-				'300': 'fff',
-				'400': 'fff',
-				'500': 'fff',
-				'600': 'e53935',
-				'700': 'd32f2f',
-				'800': 'c62828',
-				'900': 'b71c1c',
-				'A100': 'ff8a80',
-				'A200': 'ff5252',
-				'A400': 'ff1744',
-				'A700': 'd50000' // could also specify this if default was 'dark'
-			});
-			$mdThemingProvider.theme('custom')
-				.primaryPalette('whitePalette')
-				.accentPalette('blue-grey')
-				.warnPalette('red')
-		});
+    angular.module('appMain', ['ngRoute', 'ngMaterial', 'ngAnimate', 'ui.sortable'])
+        .config(function ($mdThemingProvider) {
+            
+            $mdThemingProvider.definePalette('custom-blue', {
+                '50': '2B2E4A',
+                '100': '2B2E4A',
+                '200': '2B2E4A',
+                '300': '2B2E4A',
+                '400': '2B2E4A',
+                '500': '2B2E4A',
+                '600': '2B2E4A',
+                '700': '2B2E4A',
+                '800': '2B2E4A',
+                '900': '2B2E4A',
+                'A100': '2B2E4A',
+                'A200': '2B2E4A',
+                'A400': '2B2E4A',
+                'A700': 'd50000',
+                'contrastDefaultColor': 'light'
+            });
+            $mdThemingProvider.definePalette('custom-red', {
+                '50': 'E84545',
+                '100': 'E84545',
+                '200': 'E84545',
+                '300': 'E84545',
+                '400': 'E84545',
+                '500': 'E84545',
+                '600': 'E84545',
+                '700': 'E84545',
+                '800': 'c62828',
+                '900': 'E84545',
+                'A100': 'E84545',
+                'A200': 'E84545',
+                'A400': 'E84545',
+                'A700': 'd50000',
+                'contrastDefaultColor': 'light' 
+            });
+
+            $mdThemingProvider.theme('default')
+                .primaryPalette('custom-blue')
+                .accentPalette('custom-red')
+                .warnPalette('custom-blue')
+                .backgroundPalette('grey');
+                
+            $mdThemingProvider.definePalette('white-palette', {
+                '50': 'fff',
+                '100': 'fff',
+                '200': 'fff',
+                '300': 'fff',
+                '400': 'fff',
+                '500': 'fff',
+                '600': 'e53935',
+                '700': 'd32f2f',
+                '800': 'c62828',
+                '900': 'b71c1c',
+                'A100': 'ff8a80',
+                'A200': 'ff5252',
+                'A400': 'ff1744',
+                'A700': 'd50000' // could also specify this if default was 'dark'
+            });
+
+            $mdThemingProvider.theme('custom')
+                .primaryPalette('white-palette')
+                .accentPalette('custom-blue')
+                .warnPalette('red')
+        });
 })(angular);
--- app/controllers/trainningController.js
+++ app/controllers/trainningController.js
@@ -5,15 +5,27 @@
         .module('appMain')
         .controller('TrainningController', TrainningController);
 
-    TrainningController.$inject = ['$location', '$mdDialog'];
+    TrainningController.$inject = ['$location', '$mdDialog', '$mdSidenav', '$mdMedia'];
 
-    function TrainningController($location, $mdDialog) {
+    function TrainningController($location, $mdDialog, $mdSidenav, $mdMedia) {
         var vm = this;
 
         vm.activeStep = 1;
         vm.steps = [{ title: "Informações do treino", subTitle: null, active: "true" }, { title: "Exercícios", subTitle: null, active: "false" }, { title: "Alunos", subTitle: "Adicione o treino para seus alunos", active: "false" }]
-        vm.setActive = setActive;
+        vm.filterSelected = true;
         vm.stepIsActive = stepIsActive;
+        vm.exercises = [{ id: 1, name: 'Supino reto' }, { id: 2, name: "Supino horizontal" }, { id: 3, name: "Supino vertical" }, { id: 4, name: "Fly reto" }, { id: 5, name: "Barra invertida" }, { id: 6, name: "Bíceps concentrado" }]
+        vm.exerciseList = []
+        vm.allowEdit = false;
+
+        vm.addExercise = addExercise;
+        vm.querySearch = querySearch;
+        vm.allContacts = loadContacts();
+        vm.setActive = setActive;
+        vm.show = show;
+        vm.showAdvanced = showAdvanced;
+
+        vm.contacts = [vm.allContacts[0]];
 
         function setActive(step) {
             vm.activeStep = step;
@@ -27,15 +39,12 @@
             else
                 return false;
         }
-        
-        // function stepHasError(step) {
-        //     // if()
-        // }
 
-        vm.querySearch = querySearch;
-        vm.allContacts = loadContacts();
-        vm.contacts = [vm.allContacts[0]];
-        vm.filterSelected = true;
+        function addExercise() {
+            vm.exerciseSelected.series = 4;
+            vm.exerciseSelected.replays = 10;
+            vm.exerciseList.push(vm.exerciseSelected);
+        }
         /**
          * Search for contacts.
          */
@@ -77,5 +86,79 @@
             });
         }
 
+        function show(edit, exercise, $event) {
+            showDialog(exercise, edit, $event);
+        }
+
+        function showDialog(exercise, edit, $event) {
+            var parentEl = angular.element(document.body);
+            $mdDialog.show({
+                parent: parentEl,
+                targetEvent: $event,
+                templateUrl: "app/views/partials/dialogs/exercise.html",
+                locals: {
+                    exercise: exercise,
+                    allowEdit: edit
+                },
+                controller: DialogController,
+                fullscreen: ($mdMedia('sm') || $mdMedia('xs'))
+            });
+            function DialogController($scope, $mdDialog, exercise, allowEdit) {
+
+                $scope.exercise = exercise;
+                $scope.allowEdit = edit;
+
+                $scope.closeDialog = function () {
+                    $mdDialog.hide();
+                }
+            }
+        }
+
+        function showAdvanced(item, $event) {
+            var parentEl = angular.element(document.body);
+            $mdDialog.show({
+                parent: parentEl,
+                targetEvent: $event,
+                templateUrl: "app/views/partials/dialogs/advancedExercise.html",
+                locals: {
+                    exercise: item,
+                    exerciseList: vm.exercises
+                },
+                controller: DialogController,
+                fullscreen: ($mdMedia('sm') || $mdMedia('xs'))
+            });
+            function DialogController($scope, $mdDialog, exercise, exerciseList) {
+
+                $scope.exercise = exercise;
+                $scope.exerciseList = exerciseList;
+                $scope.exercises = [];
+                $scope.advancedExercise = {};
+                
+                $scope.exercises.push($scope.exercise)
+                $scope.advancedExercise.name = $scope.exercise.name + " +";
+                
+                $scope.closeDialog = function () {
+                    $mdDialog.hide();
+                }
+
+                $scope.addExercise = function () {
+                    $scope.exerciseSelected.series = 4;
+                    $scope.exerciseSelected.replays = 10;
+                    $scope.exercises.push($scope.exerciseSelected);
+                }
+                
+                $scope.edit = function (ex) {
+                    console.log(ex);
+                    $scope.editExercise = ex;
+                    console.log($scope.editExercise);
+                }
+                
+                $scope.finishEdit = function(){
+                    console.log($scope.editExercise);
+                    $scope.editExercise = null;
+                    console.log($scope.editExercise);
+                }
+            }
+        }
     }
 })();
