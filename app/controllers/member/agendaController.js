59555891fb5059e56508a1f365abf8172e589e4c
Gabriel Malinosqui <Gabriel Malinosqui>
2016-01-23 03:12:40 -0200

    finishing agenda view

--- app/controllers/member/agendaController.js
+++ app/controllers/member/agendaController.js
@@ -5,19 +5,24 @@
         .module('appMain')
         .controller('AgendaController', AgendaController);
 
-    AgendaController.$inject = ['$http'];
+    AgendaController.$inject = ['$http', '$mdToast'];
 
-    function AgendaController($http) {
+    function AgendaController($http, $mdToast) {
 
         var vm = this;
 
-        vm.days = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"];
+        vm.days = ["Seg", "Ter", "Quar", "Qui", "Sex", "Sab", "Dom"];
 
         $http.get('aux/trainnings.json').then(function (res) {
-            vm.draggableObjects = res.data;
-            
-            vm.droppedObjects1 = vm.draggableObjects;
+            vm.avaibleTrainnings = res.data;
+
+            vm.droppedObjects1 = [];
             vm.droppedObjects2 = [];
+            vm.droppedObjects3 = [];
+            vm.droppedObjects4 = [];
+            vm.droppedObjects5 = [];
+            vm.droppedObjects6 = [];
+            vm.droppedObjects7 = [];
 
             vm.onDropComplete1 = function (data, evt) {
                 var index = vm.droppedObjects1.indexOf(data);
@@ -25,43 +30,62 @@
                 data.day = dayOfTheWeek;
                 if (index == -1)
                     vm.droppedObjects1.push(data);
+
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent('Não esqueça de salvar suas alterações')
+                        .position('top right')
+                        .hideDelay(3000)
+                    );
             }
+
             vm.onDragSuccess1 = function (data, evt) {
+                console.log(">>>", "onDragSuccess1");
                 var index = vm.droppedObjects1.indexOf(data);
                 if (index > -1) {
                     vm.droppedObjects1.splice(index, 1);
                 }
             }
+
+            vm.deleteFrom1 = function ($index) {
+                vm.droppedObjects1.splice($index, 1);
+            }
+
             vm.onDropComplete2 = function (data, evt) {
-                console.log(data);
                 var index = vm.droppedObjects2.indexOf(data);
                 var dayOfTheWeek = vm.days[1];
                 data.day = dayOfTheWeek;
                 if (index == -1)
                     vm.droppedObjects2.push(data);
             }
+
             vm.onDragSuccess2 = function (data, evt) {
+                console.log(">>>", "onDragSuccess2 ");
                 var index = vm.droppedObjects2.indexOf(data);
                 if (index > -1) {
-                    vm.droppedObjects2.splice(index, 1);
+                    vm.droppedObjects1.splice(index, 1);
                 }
             }
+
             vm.onDropComplete3 = function (data, evt) {
                 var index = vm.droppedObjects3.indexOf(data);
-                var dayOfTheWeek = vm.$parent.days[2];
+                var dayOfTheWeek = vm.days[2];
                 data.day = dayOfTheWeek;
                 if (index == -1)
                     vm.droppedObjects3.push(data);
             }
+
             vm.onDragSuccess3 = function (data, evt) {
+                console.log(">>>", "onDragSuccess3");
                 var index = vm.droppedObjects3.indexOf(data);
                 if (index > -1) {
                     vm.droppedObjects3.splice(index, 1);
                 }
             }
+
             vm.onDropComplete4 = function (data, evt) {
                 var index = vm.droppedObjects4.indexOf(data);
-                var dayOfTheWeek = vm.$parent.days[3];
+                var dayOfTheWeek = vm.days[3];
                 data.day = dayOfTheWeek;
                 if (index == -1)
                     vm.droppedObjects4.push(data);
@@ -72,9 +96,10 @@
                     vm.droppedObjects4.splice(index, 1);
                 }
             }
+
             vm.onDropComplete5 = function (data, evt) {
                 var index = vm.droppedObjects5.indexOf(data);
-                var dayOfTheWeek = vm.$parent.days[4];
+                var dayOfTheWeek = vm.days[4];
                 data.day = dayOfTheWeek;
                 if (index == -1)
                     vm.droppedObjects5.push(data);
@@ -85,6 +110,35 @@
                     vm.droppedObjects5.splice(index, 1);
                 }
             }
+
+            vm.onDropComplete6 = function (data, evt) {
+                var index = vm.droppedObjects6.indexOf(data);
+                var dayOfTheWeek = vm.days[5];
+                data.day = dayOfTheWeek;
+                if (index == -1)
+                    vm.droppedObjects6.push(data);
+            }
+            vm.onDragSuccess6 = function (data, evt) {
+                var index = vm.droppedObjects5.indexOf(data);
+                if (index > -1) {
+                    vm.droppedObjects6.splice(index, 1);
+                }
+            }
+
+            vm.onDropComplete7 = function (data, evt) {
+                var index = vm.droppedObjects7.indexOf(data);
+                var dayOfTheWeek = vm.days[6];
+                data.day = dayOfTheWeek;
+                if (index == -1)
+                    vm.droppedObjects7.push(data);
+            }
+            vm.onDragSuccess7 = function (data, evt) {
+                var index = vm.droppedObjects5.indexOf(data);
+                if (index > -1) {
+                    vm.droppedObjects7.splice(index, 1);
+                }
+            }
+
             vm.getData = function (data, element) {
                 vm.obj = data;
                 var showOverlay = angular.element(document.querySelector('.overlay'));
